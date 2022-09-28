913b7672da78ab8e82fe426abd16ef7a0738512a
Valery Sushkevich <vsushkevich@elinext.com>
2022-09-28 21:38:04 +0300

    file size fix

--- Dto/Archive.cs
+++ Dto/Archive.cs
@@ -11,7 +11,7 @@ namespace GlacierApp
         public string ArchiveId { get; set; }
         public string ArchiveDescription { get; set; }
         public DateTime CreationDate { get; set; }
-        public int Size { get; set; }
+        public long Size { get; set; }
         public string SHA256TreeHash { get; set; }
     }
 }
